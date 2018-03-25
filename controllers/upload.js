3483fc85723f34773a8b01e0449fc1029cd6b656
KirstynNatavio <kirstynatavio@hotmail.com>
2018-03-24 21:17:08 -0400

    Add upload page

---
+++ controllers/upload.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Redirect = require('../middlewares/redirect');
+
+module.exports = {
+  registerRouter() {
+    const router = express.Router();
+
+    router.get('/', this.index);
+
+    return router;
+  },
+  index(req, res) {
+    res.render('upload');
+  },
+};
