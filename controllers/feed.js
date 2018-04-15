4c263e86194d653d10110efe45eebf30409f9370
KirstynNatavio <kirstynatavio@hotmail.com>
2018-04-14 23:35:46 -0400

    Fix upload of content to Google Cloud Storage and metadata to database

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,7 +5,7 @@ module.exports = {
   registerRouter() {
     const router = express.Router();
 
-    router.get('/', this.index);
+    router.get('/', Redirect.ifNotLoggedIn(), this.index);
 
     return router;
   },
--- controllers/upload.js
+++ controllers/upload.js
@@ -5,6 +5,7 @@ const multer = require('multer')
 const Storage = require('@google-cloud/storage')
 const storage = Storage()
 const format = require('util').format;
+const path = require('path')
 
 // Set storage Engine
 const storageMulter = multer.diskStorage({
@@ -23,8 +24,8 @@ module.exports = {
   registerRouter() {
     const router = express.Router();
 
-    router.get('/', this.index);
-    router.post('/', this.upload);
+    router.get('/', Redirect.ifNotLoggedIn(), this.index);
+    router.post('/', Redirect.ifNotLoggedIn(), this.upload);
 
     return router;
   },
@@ -32,62 +33,46 @@ module.exports = {
     res.render('upload');
   },
   upload(req, res) {
+
+    const database = firebase.database();
     upload(req, res, (err) => {
       if (err) {
         res.render('upload', {err})
       }
       else {
+        
+        
         const bucket = storage.bucket("gs://teachxchange-5bff2.appspot.com");
         const blob = bucket.file(req.file.originalname);
-        const user = firebase.auth().currentUser;
-        if (user) {
-          console.log("User is logged in")
-        }
-        else {
-          console.log("ARGGANSDFAKSFDAKFBDA")
-        }
         const blobStream = blob.createWriteStream();
-
-
-         blobStream.on('finish', () => {
+        console.log(blobStream)
+        blobStream.on('finish', () => {
         // The public URL can be used to directly access the file via HTTP.
+          const filename = blob.name;
+          const childPath = filename.substr(0,filename.lastIndexOf('.')) + '-' + Date.now();
           const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-          res.status(200).send(publicUrl);
+          database.ref(`pictures/${childPath}`).set({
+            'inputType': req.body.inputType,
+            'uploadedBy': firebase.auth().currentUser.uid,
+            'gradeLevel': req.body.gradelevel,
+            'subject': req.body.subject,
+            'note': req.body.note,
+            'publicUrl': publicUrl
+          })
+          res.status(200).redirect('feed');
         });
 
         blobStream.end(req.file.buffer);
 
+        
 
-        // const storageRef = storage.ref('uploads/');
-        // const uploadsRef = firebase.database().ref('uploads/'); 
-        // const file =  req.file;  
-        // const uploadsMetadata = {
-        //   customMetadata: {
-        //     'inputType': fileType,
-        //     'uploadedBy': firebase.auth().currentUser.uid,
-        //     'gradelevel': gradeLevel,
-        //     'subject': subject,
-        //     'note': note
-        //  }
 
-        // }
         // const uploadTask = storageRef.child(file.originalname).put(file, uploadsMetadata);
         // return new Promise((resolve, reject) => {
         //   uploadTask.on('state_changed', function(snap) {
         //     console.log('Progress: ', nap.bytesTransferred, '/', snap.totalBytes, ' bytes');
 
-        //   }, function(err) {
-        //     console.log('upload error', err);
-        //     reject(err);
-        //   }, function() {
         //     const metadata = uploadTask.snapshot.metadata;
-
-        //   })
-        // })
-
-       
-        // const filename = req.file.originalname;
-        // console.log(typeof(req.file))
         // const storageRef = firebase.storage().ref().child('Images/' + filename).put(file);
 
         // uploadTask.on('state_changed'), function(snapshot) {
@@ -103,16 +88,9 @@ module.exports = {
        
         // const uploadTask = firebase.storage().ref().child('Picture/hackathon.JPG').put(test).then(function(snapshot) {
         //   console.log('Uploaded a blob or file!');
-
-        // }).catch((err) => console.log(err))
-        //res.send('test')
       }
     })
     
-    // const fileType = req.body.inputType;
-    // const gradeLevel = req.body.gradelevel; 
-    // const subject = req.body.subject;
-    // const note = req.body.note;
     
     
   }
